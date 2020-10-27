5ab8b4a0aa088c026829eb584a1390bbd6fa9732
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-27 11:54:35 -0300

    Creates route handler for deleting repository

--- src/app.js
+++ src/app.js
@@ -55,8 +55,20 @@ app.put('/repositories/:id', (request, response) => {
   return response.json(updatedRepository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    repository => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exist.' });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
