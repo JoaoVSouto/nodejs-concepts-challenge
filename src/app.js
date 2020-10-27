6b84031692c6ef2a3770725c1e14c5fc4c7d9a12
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-27 12:01:29 -0300

    Creates route handler for creating a new like on repository

--- src/app.js
+++ src/app.js
@@ -71,8 +71,18 @@ app.delete('/repositories/:id', (request, response) => {
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find(r => r.id === id);
+
+  if (!repository) {
+    return response.status(400).json({ error: 'Repository does not exist.' });
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
