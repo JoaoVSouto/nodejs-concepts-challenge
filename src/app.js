986158d3bbf019dfd3667e91a1a1d5668325d9a4
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-27 11:53:50 -0300

    Creates route handler for updating repository

--- src/app.js
+++ src/app.js
@@ -30,8 +30,29 @@ app.post('/repositories', (request, response) => {
   return response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(
+    repository => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exist.' });
+  }
+
+  const updatedRepository = {
+    ...repositories[repositoryIndex],
+    id,
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositoryIndex] = updatedRepository;
+
+  return response.json(updatedRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
